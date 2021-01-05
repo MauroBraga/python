4a8b11834d4333b2b80fb8494af882b12a8253fe
MauroBraga <mrb0305@gmail.com>
2021-01-05 10:09:11 -0300

    Estruturas de repetição

---
+++ estruturasCondicionaisElseIf.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+x = 1
+y = 2
+
+if x == y:
+    print("números iguais")
+elif y < x:
+    print("x mmenor que y")     
+elif y > x:
+    print("y maior que x")    
+else:
+    print("números diferentes")
