254065cedb076740d3f47a916ef926d7a0334988
MauroBraga <mrb0305@gmail.com>
2021-01-04 23:41:29 -0300

    Comentário e codificação UTF-8

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print("Ola Mundo!");
+# -*- coding: utf8 -*-
+# isto é um comentário
+print("Olá Mundo!")
+print("Outra mensagem")
+print("mensagem 3")
+
+#outro comentario
+print("Outro comentario")
+
+"""
+comentario 
+de muitas
+linhas
+"""
