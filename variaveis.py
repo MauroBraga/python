a34e706eb0c68e3b10b3a741a1af6a0aa69559ee
MauroBraga <mrb0305@gmail.com>
2021-01-05 00:06:32 -0300

    Variáveis

---
+++ variaveis.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+minha_varavel = "Olá Mundo"
+
+print(minha_varavel)
+print(minha_varavel)
+print(minha_varavel)
+
+var1 = 1 # variável inteira
+var2 = 1.1 # variável float
+var3 = "Eu sou uma string" # variável string
+var4 =True
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
