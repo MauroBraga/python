facc07350bd2009cc6949eed605f63f8277c0bfe
MauroBraga <mrb0305@gmail.com>
2021-01-06 14:17:42 -0300

    Arquivos

---
+++ arquivo.py
@@ -0,0 +1,20 @@
+arquivo = open("arquivo.txt")
+
+print(arquivo)
+
+# linhas = arquivo.readlines()
+
+# print(linhas)
+
+# for linha in linhas:
+#     print(linha)
+
+# texto_completo = arquivo.read()
+
+# print(texto_completo)
+
+w = open("arquivo2.txt","a")
+
+w.write("Esse eh meu arquivo \n")
+
+w.close()
