74b36ad8eff50f9bc459b5ec67dacf3b34fe702f
MauroBraga <mrb0305@gmail.com>
2021-01-07 08:50:32 -0300

    Dicionários

---
+++ dicionario.py
@@ -0,0 +1,15 @@
+meu_dicionario = {"A":"AMEIXA","B":"BOLA","C":"CACHORRO"}
+
+print(meu_dicionario["A"])
+
+for chave in meu_dicionario:
+    print(chave, meu_dicionario[chave])
+
+for i in meu_dicionario.items():
+    print(i)
+
+for i in meu_dicionario.values():
+    print(i)
+
+for i in meu_dicionario.keys():
+    print(i)    
---
+++ lista2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf8 -*-
+lista = [124,345,5,72,46,6,7,3,1,7,0]
+
+# lista.sort()
+
+# lista = sorted(lista)
+
+# lista.sort(reverse=True)
+lista.reverse()
+
+print(lista)
+
+lista2 = ["bola", "abacate", "dinheiro"]
+
+lista2.sort()
+
+print(lista2)
