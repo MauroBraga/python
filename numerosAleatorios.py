9bc4394e609fdcd59561a800c084bdb511015030
MauroBraga <mrb0305@gmail.com>
2021-01-07 09:33:26 -0300

    tratamento de excecao

---
+++ numerosAleatorios.py
@@ -0,0 +1,7 @@
+import random 
+
+print(random.randint(0,10))
+
+lista = [6, 45, 9]
+numero = random.choice(lista)
+print(numero)
---
+++ tratamentoExcecao.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+a = 2
+b = 0
+
+try:
+    print(a/b)
+except:
+    print("Não é permitido divisão por 0")
